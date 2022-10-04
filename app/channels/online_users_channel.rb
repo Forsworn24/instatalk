916daaea97e53fd2406b196babb3b12aa72bfcc0
Forsworn24 <mudinov.ali2401@gmail.com>
2022-10-05 02:10:42 +0300

    refactor commit

--- app/channels/online_users_channel.rb
+++ app/channels/online_users_channel.rb
@@ -31,22 +31,4 @@ class OnlineUsersChannel < ApplicationCable::Channel
   def nickname
     current_user.nickname
   end
-
-
-
-  #def subscribed
-  #  stream_from 'online_users_channel'
-  #  current_user.update(online: true)
-  #  broadcast  
-  #end
-
-  #def unsubscribed
-  #  current_user.update(online: false)
-  #  broadcast
-  #end
-
-  #def broadcast
-  #  ActionCable.server.broadcast 'online_users_channel',
-  #                               user: current_user.as_json
-  #end
 end
